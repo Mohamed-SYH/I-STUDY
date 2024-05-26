a63765ef5456a5e3963f6c3a29ee79d6cbb66f7f
Mohamed-SYH <mahamedsayah.1234@gmail.com>
2024-05-26 23:11:39 +0100

    updated staticfile

--- src/settings.py
+++ src/settings.py
@@ -106,11 +106,12 @@ AUTH_USER_MODEL = 'profiles.User'
 
 # Static files (CSS, JavaScript, Images)
 STATIC_URL = '/static/'
-
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATICFILES_DIRS = [
     os.path.join(BASE_DIR, 'static'),
-    os.path.join(BASE_DIR, 'media'),
+    os.path.join(BASE_DIR, 'media'),  # Add this line to include the 'media' directory
 ]
+
 
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media/')
